f3ec427dbdba45c4e4e224d19993ae1bc7757c9e
wex335 <wex335@yandex.ru>
2022-04-10 20:16:17 +0300

    dimon`s edit

--- main.py
+++ main.py
@@ -1,28 +1,31 @@
 import os.path
 import sbeaver
-server = sbeaver.Server(port=8001)
+server = sbeaver.Server(port=8000)
 import re
-print(re.fullmatch(r'/(\S*)', '/index.html'))
 
 @server.sbind('/favicon.ico')
 def fav(req):
-    return sbeaver.file('icon.svg', sbeaver.Types.image.svg)
+    return sbeaver.redirect(301, '/assets/images/svg/icon.svg')
 
-@server.bind(r'/(\w+)\.(\w+)')
-def index(req, filename = 'index', ex = 'html'):
-    if os.path.exists(filename+'.'+ex):
+
+@server.bind(r'/(.*)')
+def alll(req, filename = 'index.html'):
+    ex = re.split('\.', req.path)[-1]
+    if os.path.exists(filename):
         if ex in sbeaver.Types.image.__dict__:
-            return sbeaver.file(filename+'.'+ex, sbeaver.Types.image.__dict__[ex])
+            return sbeaver.file(filename, sbeaver.Types.image.__dict__[ex])
         elif ex == 'html':
-            with open(filename+'.'+ex, 'r') as f:
+            with open(filename, 'r') as f:
                 return 200, f.read(-1)
         elif ex in sbeaver.Types.text.__dict__:
-            return sbeaver.file(filename+'.'+ex, sbeaver.Types.text.__dict__[ex])
+            return sbeaver.file(filename, sbeaver.Types.text.__dict__[ex])
         else:
-            with open(filename+'.'+ex, 'rb') as f:
+            with open(filename, 'rb') as f:
                 return 200, f.read(-1)
     with open('404.html') as f:
         return 404, f.read(-1)
+
+
 
 
 @server.code404()
@@ -30,5 +33,5 @@ def pnf(request):
     print(404)
     with open('404.html') as f:
         return f.read(-1)
-print(server.__dict__)
+
 server.start()
